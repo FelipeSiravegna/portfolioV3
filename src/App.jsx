76aeefe500661a206366c507a12695304b0f935d
Felipe Siravegna <felipesiravegna1@gmail.com>
2023-04-17 15:40:53 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Felipe Siravegna</h1>
+      <h4>Full Stack Developer</h4>
+    </div>
+  )
+}
+
+export default App
