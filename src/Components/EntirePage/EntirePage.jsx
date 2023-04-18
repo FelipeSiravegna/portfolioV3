58845334b68f977b39960ee94cdac7424ea8e302
Felipe Siravegna <felipesiravegna1@gmail.com>
2023-04-18 14:44:53 -0300

    Welcome id added to Welcome component

--- src/Components/EntirePage/EntirePage.jsx
+++ src/Components/EntirePage/EntirePage.jsx
@@ -1,26 +1,28 @@
-import style from './EntirePage.module.css'
-import Welcome from '../Welcome/Welcome.jsx'
-import Skills from '../Skills/Skills.jsx'
-import Projects from '../Projects/Projects.jsx'
-import About from '../About/About.jsx'
-import Contact from '../Contact/Contact.jsx'
+import style from "./EntirePage.module.css";
+import Welcome from "../Welcome/Welcome.jsx";
+import Skills from "../Skills/Skills.jsx";
+import Projects from "../Projects/Projects.jsx";
+import About from "../About/About.jsx";
+import Contact from "../Contact/Contact.jsx";
 
-export default function EntirePage(){
-    return(
-        <div>
-            <Welcome />
-            <div id="skills">
-                <Skills />
-            </div>
-            <div id="projects">
-                <Projects/>   
-            </div>
-            <div id="about">
-                <About/>
-            </div>
-            <div id="contact">
-                <Contact/>
-            </div>
-        </div>
-    )
+export default function EntirePage() {
+  return (
+    <div>
+      <div id="welcome">
+        <Welcome />
+      </div>
+      <div id="skills">
+        <Skills />
+      </div>
+      <div id="projects">
+        <Projects />
+      </div>
+      <div id="about">
+        <About />
+      </div>
+      <div id="contact">
+        <Contact />
+      </div>
+    </div>
+  );
 }
