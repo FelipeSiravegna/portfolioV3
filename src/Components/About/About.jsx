22966cb3b6fa240945ec93501a1780a411b6f749
Felipe Siravegna <felipesiravegna1@gmail.com>
2023-04-19 20:02:39 -0300

    About component set up

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,9 +1,11 @@
-import style from './About.module.css'
+import style from "./About.module.css";
 
-export default function About(){
-    return(
-        <div>
-            <h1>About</h1>
-        </div>
-    )
+export default function About() {
+  return (
+    <section id="about" className={style.container}>
+      <div className={style.insideContainer}>
+        <h2>About</h2>
+      </div>
+    </section>
+  );
 }
