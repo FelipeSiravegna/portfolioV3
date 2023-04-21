7c6c6bc822f7bd5b3bc129591ce30325217fb9b0
Felipe Siravegna <felipesiravegna1@gmail.com>
2023-04-21 16:58:50 -0300

    Contact section added

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -4,7 +4,22 @@ export default function Contact() {
   return (
     <section id="about" className={style.container}>
       <div className={style.insideContainer}>
-        <h2 className={style.projectsTitle}>Contact</h2>
+        <h2 className={style.contactTitle}>Contact</h2>
+        <form action="https://formsubmit.co/felipesiravegnadev@gmail.com" method="POST" className={style.form} >
+                <label for="nombre" className={style.label}>Name</label>
+                <input type="text" name="Name" className={style.input}/>
+
+                <label for="email" className={style.label}>E-Mail</label>
+                <input type="email" name="Email" className={style.input}/>
+
+                <label for="mensaje" className={style.label}>Message</label>
+                <textarea name="Message" cols="15" rows="8" className={style.messageInput}/>
+
+                <input type="submit" value="Send" className={style.button}/>
+
+                <input type="hidden" name="_next" value="https://felipesiravegnadev.vercel.app/" />
+                <input type="hidden" name="_captcha" value="false" />
+            </form>
       </div>
     </section>
   );
