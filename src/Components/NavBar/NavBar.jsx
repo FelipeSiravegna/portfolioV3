e9bf04837b2453bf155b5615138dee1aed2db14a
Felipe Siravegna <felipesiravegna1@gmail.com>
2023-04-24 18:47:51 -0300

    Responsive test

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,28 +1,29 @@
 import style from "./NavBar.module.css";
-import { Link } from "react-router-dom";
 import LINKEDIN from "../../Media/linkedin.svg";
 import GITHUB from "../../Media/github.svg";
 import RESUME from "../../Media/resume.svg";
 import CV from "../../Media/CV.pdf";
+import SKILLS from "../../Media/skills.svg";
+import PROJECTS from "../../Media/projects.svg";
+import ABOUT from "../../Media/about.svg";
+import CONTACT from "../../Media/contact.svg";
 
 export default function NavBar() {
   return (
     <nav className={style.container}>
       <div className={style.scrollLinks}>
         <a href="#skills" className={style.scrollLink}>
-          Skills
+          <img src={SKILLS} className={style.navbarImages} alt="Skills" />
         </a>
         <a href="#projects" className={style.scrollLink}>
-          Projects
+          <img src={PROJECTS} className={style.navbarImages} alt="Projects" />
         </a>
         <a href="#about" className={style.scrollLink}>
-          About
+          <img src={ABOUT} className={style.navbarImages} alt="About" />
         </a>
         <a href="#contact" className={style.scrollLink}>
-          Contact
+          <img src={CONTACT} className={style.navbarImages} alt="Contact" />
         </a>
-      </div>
-      <div className={style.linksContainer}>
         <a
           href="https://www.linkedin.com/in/felipe-siravegna/"
           target="_blank"
@@ -41,6 +42,9 @@ export default function NavBar() {
           <img src={RESUME} className={style.navbarImages} alt="Resume" />
         </a>
       </div>
+      {/* <div className={style.linksContainer}>
+        
+      </div> */}
     </nav>
   );
 }
