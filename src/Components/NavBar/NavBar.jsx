b33829ffe93b3ab429309b2bb31f3974f2d25ad7
Felipe Siravegna <felipesiravegna1@gmail.com>
2023-05-09 01:37:46 -0300

    NavBar now shows different in mobile and pc

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -11,7 +11,7 @@ import CONTACT from "../../Media/contact.svg";
 export default function NavBar() {
   return (
     <nav className={style.container}>
-      <div className={style.scrollLinks}>
+      <div className={style.scrollLinksMobile}>
         <a href="#skills" className={style.scrollLink}>
           <img src={SKILLS} className={style.navbarImages} alt="Skills" />
         </a>
@@ -42,9 +42,41 @@ export default function NavBar() {
           <img src={RESUME} className={style.navbarImages} alt="Resume" />
         </a>
       </div>
-      {/* <div className={style.linksContainer}>
-        
-      </div> */}
+      <div className={style.scrollLinksPC}>
+        <div className={style.scrollLinksInside}>
+          <a href="#skills" className={style.scrollLink}>
+            <h4>Skills</h4>
+          </a>
+          <a href="#projects" className={style.scrollLink}>
+            <h4>Projects</h4>
+          </a>
+          <a href="#about" className={style.scrollLink}>
+            <h4>About</h4>
+          </a>
+          <a href="#contact" className={style.scrollLink}>
+            <h4>Contact</h4>
+          </a>
+        </div>
+        <div className={style.scrollLinksOutside}>
+          <a
+            href="https://www.linkedin.com/in/felipe-siravegna/"
+            target="_blank"
+            className={style.link}
+          >
+            <img src={LINKEDIN} className={style.navbarImages} alt="LinkedIn" />
+          </a>
+          <a
+            href="https://github.com/FelipeSiravegna"
+            target="_blank"
+            className={style.link}
+          >
+            <img src={GITHUB} className={style.navbarImages} alt="GitHub" />
+          </a>
+          <a href={CV} download="Felipe Siravegna Resume">
+            <img src={RESUME} className={style.navbarImages} alt="Resume" />
+          </a>
+        </div>
+      </div>
     </nav>
   );
 }
